b98de763801ad8410749e283898879f343db6b24
Lucas Lopez <159229272+lucastricanico@users.noreply.github.com>
2026-09-25 15:19:20 -0400

    Complete Ruby Introduction

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,43 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+  arr.combination(2).any? { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  s.match?(/\A[b-df-hj-np-tv-z]/i)
 end
 
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  s.match?(/\A[01]+\z/) && s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-  # YOUR CODE HERE
+   attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
 end
